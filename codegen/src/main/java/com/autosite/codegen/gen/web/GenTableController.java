7ead281ca6ac2be8bf300c57995c1722dce7e7ef
lyh <1486724367@qq.com>
2021-01-29 16:13:56 +0800

    引入bootstrap-table

--- codegen/src/main/java/com/autosite/codegen/gen/web/GenTableController.java
+++ codegen/src/main/java/com/autosite/codegen/gen/web/GenTableController.java
@@ -4,11 +4,16 @@ package com.autosite.codegen.gen.web;
 import com.autosite.codegen.common.base.BaseController;
 import com.autosite.codegen.gen.entity.GenTable;
 import com.autosite.codegen.gen.service.impl.GenTableService;
+import com.autosite.common.collect.ListUtils;
+import com.autosite.common.collect.MapUtils;
 import com.baomidou.mybatisplus.extension.plugins.pagination.Page;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
+
+import java.util.List;
+import java.util.Map;
 
 /**
  * <p>
@@ -36,8 +41,30 @@ public class GenTableController extends BaseController {
         return renderResult("true","success",page);
     }
 
+    @RequestMapping("/findList1")
+    @ResponseBody
+    public String findList1(){
+        List<Map<String,String>> list = ListUtils.newArrayList();
+        Map<String,String> map = MapUtils.newHashMap();
+        map.put("testId","111");
+        map.put("testName","测试");
+        map.put("testPassword","测试");
+        list.add(map);
+        Map<String,String> map1 = MapUtils.newHashMap();
+        map1.put("testId","222");
+        map1.put("testName","测试");
+        map1.put("testPassword","测试");
+        list.add(map1);
+        return renderResult("success","成功",list);
+    }
+
     @RequestMapping("/list")
     public String list(){
+        return "gen/genList";
+    }
+
+    @RequestMapping("/index")
+    public String index(){
         return "themes/index";
     }
 }
