ffadbd70f9153ba9c0ccb4d9a8d6d3f8d751c3a9
lyh <1486724367@qq.com>
2021-05-12 13:38:35 +0800

    修复bug

--- common/src/main/java/com/autosite/utils/SpringUtils.java
+++ common/src/main/java/com/autosite/utils/SpringUtils.java
@@ -5,7 +5,9 @@ import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.DisposableBean;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.ApplicationContextAware;
+import org.springframework.stereotype.Component;
 
+@Component
 public class SpringUtils implements ApplicationContextAware, DisposableBean {
     private static Logger logger = LoggerFactory.getLogger(SpringUtils.class);
     private static ApplicationContext applicationContext = null;
@@ -33,6 +35,7 @@ public class SpringUtils implements ApplicationContextAware, DisposableBean {
         clearHolder();
     }
 
+    @Override
     public void setApplicationContext(ApplicationContext applicationContext) {
         SpringUtils.applicationContext = applicationContext;
     }
